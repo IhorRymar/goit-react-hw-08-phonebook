d985cc44355ac87d66cf011bd564508131e50e5e
IhorRymar <95086464+IhorRymar@users.noreply.github.com>
2022-09-18 04:30:40 +0300

    add basename

--- src/index.js
+++ src/index.js
@@ -15,9 +15,7 @@ root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter
-      // basename="/goit-react-hw-08-phonebook/"
-      >
+      <BrowserRouter basename="/goit-react-hw-08-phonebook/">
         <App />
       </BrowserRouter>
     </PersistGate>
